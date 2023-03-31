748781c543328603026c405f2a9a91312f444a87
k-chandravir <kumar.chandravir@m2pfintech.com>
2023-03-31 14:39:07 +0530

    Req1: Add. Added Tests And Functions For The Checking Two Equal Probabilities

--- src/main/java/com/m2p/Probability.java
+++ src/main/java/com/m2p/Probability.java
@@ -1,4 +1,21 @@
 package com.m2p;
 
 public class Probability {
+    private final double probabilityOfEvent;
+    public Probability(double probabilityOfTheEvent) {
+        this.probabilityOfEvent = probabilityOfTheEvent;
+    }
+
+    @Override
+    public boolean equals(Object probabilityObject){
+        if(this == probabilityObject){
+            return true;
+        }
+        if(this.getClass() != probabilityObject.getClass()){
+            return false;
+        }
+
+        Probability that = (Probability) probabilityObject;
+        return this.probabilityOfEvent == that.probabilityOfEvent;
+    }
 }
--- src/test/java/com/m2p/ProbabilityTest.java
+++ src/test/java/com/m2p/ProbabilityTest.java
@@ -1,4 +1,18 @@
 package com.m2p;
 
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ProbabilityTest {
+    @Test
+    void toReturnZeroPointFiveWhenHeadIsInput(){
+        Probability headProbability = new Probability(0.5);
+
+        Probability tailProbability = new Probability(0.5);
+
+        assertThat(headProbability, is(equalTo(tailProbability)));
+    }
 }
